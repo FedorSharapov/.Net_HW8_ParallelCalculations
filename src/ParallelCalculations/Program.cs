886602c0670aa3aeddd2f54e7ab5e80dabf5962f
Федор Шарапов <sharapovfedor@yandex.ru>
2022-10-18 00:34:54 +0300

    обычная сумма

--- src/ParallelCalculations/Program.cs
+++ src/ParallelCalculations/Program.cs
@@ -1,9 +1,31 @@
-﻿namespace ParallelCalculations
+﻿using System.Diagnostics;
+
+namespace ParallelCalculations
 {
-    internal class Program
+    public class Program
     {
+        static int _arraySize = 10000000;
+        static int _numThreads = 4;
+
+        // ToDo: сделать рефакторинг
         static void Main(string[] args)
         {
+            if (args != null && args.Length == 1 && !int.TryParse(args[0], out _arraySize))
+            {
+                ConsoleHelper.WriteLineError("Invalid input argument.");
+                return;
+            }
+
+            // генерируем коллекцию чисел
+            IEnumerable<long> numbers = new long().Range(_arraySize);
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            ConsoleHelper.WriteLine($"Размер массива: [{_arraySize}]\r\nМетод вычеления: [обычный]");
+            long result = numbers.Sum();
+            stopwatch.Stop();
+            ConsoleHelper.WriteLine($"Время выполнения: [{stopwatch.ElapsedMilliseconds}]\r\nРезультат = [{result}]\r\n");
+
             Console.ReadKey();
         }
     }
