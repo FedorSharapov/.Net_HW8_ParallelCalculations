a627ab4855fa47646cff56f5356e1b4f1860f2b8
Федор Шарапов <sharapovfedor@yandex.ru>
2022-10-18 00:33:50 +0300

    расширение Range для Long

---
+++ src/ParallelCalculations/EnumerableExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace ParallelCalculations
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<long> Range(this long source, long length)
+        {
+            for (long i = source; i < length; i++)
+                yield return i;
+        }
+    }
+}
